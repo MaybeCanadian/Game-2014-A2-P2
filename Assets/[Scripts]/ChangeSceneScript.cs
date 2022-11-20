6a93ddc7d5399b1d879336da5b44b54da0a52207
MaybeCanadian <1coffeye@gmail.com>
2022-11-20 09:02:54 -0500

    added header to change scene script.

--- Assets/[Scripts]/ChangeSceneScript.cs
+++ Assets/[Scripts]/ChangeSceneScript.cs
@@ -2,6 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//--------------------------------------------
+//ChangeSceneScript.cs - Evan Coffey - 101267129
+//
+//Allows user to easily change the scene with space bar.
+//
+//latest revision - 11/20/2022
+//
+//Revision History- 
+//11/20/2022
+//--------------------------------------------
+
 
 public class ChangeSceneScript : MonoBehaviour
 {
